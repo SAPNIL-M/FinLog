8241fad3f5124edddee7b988144f51ed175c2ed0
SAPNIL-M <sapnilm670@gmail.com>
2025-01-30 14:42:45 +0530

    added the pages mentioned in the navigation bar

---
+++ lib/view/maintab/spending_budgets_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SpendingBudgetsPage extends StatefulWidget {
+  const SpendingBudgetsPage({super.key});
+
+  @override
+  State<SpendingBudgetsPage> createState() => _SpendingBudgetsPageState();
+}
+
+class _SpendingBudgetsPageState extends State<SpendingBudgetsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ lib/view/maintab/your_cards.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class YourCards extends StatefulWidget {
+  const YourCards({super.key});
+
+  @override
+  State<YourCards> createState() => _YourCardsState();
+}
+
+class _YourCardsState extends State<YourCards> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
